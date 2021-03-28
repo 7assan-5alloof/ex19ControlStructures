979e940cda9dee748c4c17a80c94aa12e5467b3d
7assan-5alloof <hassankhalloof20@gmail.com>
2021-03-28 22:45:41 +0400

    Write program

--- main.cpp
+++ main.cpp
@@ -5,12 +5,14 @@ using namespace std;
 int main()
 {
     cout << "Press Ctrl-C to stop..." << endl;
+    cout << "TAXI FARES" << endl;
 
     while (true)
     {
         // Get distance traveled by Boston taxi, and define constants of the taxi service there (will be cooninued after getting more info on customer's trip)
         const int initialFare = 10;
-        const double farePerMeter = 0.50;
+        double hireOnRoad = 3;
+        double farePerMeter = 0.50; // Special condition for this to apply, though...
         const int airportAdditional = 20;
 
         // Get information about the customer's trip
@@ -20,25 +22,23 @@ int main()
         char airport;
         cout << "Are you taking taxi from an airport ([Y]es/[N]o)? ";
         cin >> airport;
-        char tip;
-        cout << "Are you going to gratuate the taxi driver ([Y]es/[N]o)? ";
-        cin >> tip;
         char time;
         cout << "Are you taking taxi between 6 AM and 10 PM ([Y]es/[N]o)? ";
         cin >> time;
 
         // Excluding impossible situations
-        if ((distance == 0) || ((airport != 'y') && (airport != 'Y') && (airport != 'N') && (airport != 'n')) || ((tip != 'y') && (tip != 'Y') && (tip != 'N') && (tip != 'n')))
+        if ((distance == 0) || ((airport != 'y') && (airport != 'Y') && (airport != 'N') && (airport != 'n')))
         {
             cout << "You've gave an invalid answer to one of the questions above. Please input valid ones below..." << endl;
             continue;
         }
 
         // Calculating and displaying total price
-        double totalFare = initialFare + distance * farePerMeter;
+        if ((time == 'N') || (time == 'n')) hireOnRoad += 0.50;
+        double totalFare = initialFare + hireOnRoad;
+        for (int i = distance; i % 312 == 0; i = distance - 312) totalFare += farePerMeter;
         if ((airport == 'Y') || (airport == 'y')) totalFare += airportAdditional;
-        if ((tip == 'Y') || (tip == 'y')) totalFare *= 1.1;
-        cout << "Your described trip costs AED" << totalFare << endl;
+        cout << "Your described trip costs AED " << totalFare << endl;
     }
 
     return 0;
